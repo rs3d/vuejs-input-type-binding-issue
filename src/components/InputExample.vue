43472de551816d64e4fcd30ceea72e9f265dd7b5
Robin Schmitz <r.schmitz@kapilendo.de>
2019-12-23 11:53:39 +0100

    Adding issue example for dynamic input:type binding

---
+++ src/components/InputExample.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{ checkbox }}
+    <div
+      v-for="(field, index) in [{ type: 'checkbox', value: 'TEST ' }]"
+      :key="index"
+    >
+      <!-- <input type="checkbox" :value="field.value" v-model="checkbox" /> -->
+      <!-- Works with type="checkbox" -->
+
+      <input :type="field.type" :value="field.value" v-model="checkbox" />
+      <!-- Doesn't work with dynamic binding :type="field.type" -->
+      {{ field }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputExample',
+  data: () => ({
+    checkbox: []
+  })
+};
+</script>
